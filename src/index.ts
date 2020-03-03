dad00ec9360ab9c8c6f9c3c46d8c5e493f6bc78a
nur <nur.php@gmail.com>
2020-03-03 21:04:31 +0800

    remove header

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
 // const poly = require("preact-cli/lib/lib/webpack/polyfills");
-
-import { h } from "preact";
 import habitat from "preact-habitat";
-
-import Header from "./components/header";
 import Map from "./components/map";
-
-habitat(Header).render({
-    selector: '[data-widget-host="habitat"]',
-    clean: true
-});
 
 habitat(Map).render({
     selector: '[data-widget-host="map"]',
