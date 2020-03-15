68d3b1480f43bb155df4bda05405a65a8d3046d3
nur <nur.php@gmail.com>
2020-03-16 00:49:47 +0800

    Fix short hex

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,7 @@ export const genDeviceShortAddress = (deviceKey: string | number): string => {
     if (typeof deviceKey == 'string') {
         num = parseInt(deviceKey, 10);
     }
-    return `0x${num.toString(16).toUpperCase()}`;
+
+    return `0x${(`0000${num.toString(16)}`).substr(-4).toUpperCase()}`;
 }
 export const genDeviceDetailsLink = (deviceKey: string | number): string => (`/zigbee?nwkAddr=${genDeviceShortAddress(deviceKey)}`)
