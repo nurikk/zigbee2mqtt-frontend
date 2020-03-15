6189edbc45b1eb806df65a8c2a74cea50af3b2fe
nur <nur.php@gmail.com>
2020-03-15 20:59:41 +0800

    Map fixes

--- src/components/map/consts.ts
+++ src/components/map/consts.ts
@@ -2,7 +2,8 @@ import { Device } from "../../types";
 
 
 export const GATEWAY: Device = {
-	ieeeAddr: 'Coordinator node',
+	// eslint-disable-next-line @typescript-eslint/camelcase
+	friendly_name: 'Coordinator node',
 	// eslint-disable-next-line @typescript-eslint/camelcase
 	last_seen: (Date.now() / 1000).toString(),
 	type: "Coordinator",
