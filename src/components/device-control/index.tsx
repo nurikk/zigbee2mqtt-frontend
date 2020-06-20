e1494c30a73994f41f3a69d9a7df3bc9d893146c
nur <nur.php@gmail.com>
2020-06-20 17:10:43 +0800

    Fix device actions menu

--- src/components/device-control/index.tsx
+++ src/components/device-control/index.tsx
@@ -42,36 +42,25 @@ export class DeviceControlGroup extends Component<DeviceControlGroupProps & Acti
             <div className="btn-group btn-group-sm" role="group">
                 <Button<void> className="btn btn-secondary" onClick={this.onRenameClick}><i
                     className="fa fa-edit" /></Button>
-                {
+                <div class="btn-group" role="group">
+                    <Button id="btnGroupDrop0" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i className={cx("fa", "fa-cogs")} /></Button>
+                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop0">
+                        <Button<string> class="dropdown-item" onClick={configureDevice} item={device.friendly_name}>Reconfigure</Button>
+                        <Button<string> class="dropdown-item" onClick={checkOTA} item={device.friendly_name}>Check OTA</Button>
+                    </div>
+                </div>
+                <div class="btn-group" role="group">
 
+                    <Button id="btnGroupDrop1" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i className={cx("fa", "fa-trash")} /></Button>
+                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                        {validDevice ? <Button promt class="dropdown-item" onClick={this.onRemoveClick} item={false}>Remove</Button> : null}
+                        <Button<boolean> promt class="dropdown-item" onClick={this.onRemoveClick} item={true}>Remove(force)</Button>
 
+                    </div>
+                </div>
 
-
-                    <Fragment>
-
-                        <Button class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i className={cx("fa", "fa-cogs")} /></Button>
-
-
-                        <div class="dropdown-menu" aria-labelledby="btnGroupDrop0">
-                            <Button<string> class="dropdown-item" onClick={configureDevice} item={device.friendly_name}>Reconfigure</Button>
-                            <Button<string> class="dropdown-item" onClick={checkOTA} item={device.friendly_name}>Check OTA</Button>
-                        </div>
-
-
-
-
-
-                        <Button class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i className={cx("fa", "fa-trash")} /></Button>
-
-                        <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                            {validDevice ? <Button promt class="dropdown-item" onClick={this.onRemoveClick} item={false}>Remove</Button> : null}
-                            <Button<boolean> promt class="dropdown-item" onClick={this.onRemoveClick} item={true}>Remove(force)</Button>
-
-                        </div>
-                    </Fragment>
-                }
             </div>
         );
     }
