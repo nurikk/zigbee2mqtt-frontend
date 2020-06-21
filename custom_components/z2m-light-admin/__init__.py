d6ba5eb0baee86b74ce99fe1457d803de32e9a9f
nur <nur.php@gmail.com>
2020-06-21 12:35:38 +0800

    fix

--- custom_components/z2m-light-admin/__init__.py
+++ custom_components/z2m-light-admin/__init__.py
@@ -7,15 +7,15 @@ DOMAIN = "z2m_light_admin"
 bundle_file_names = ["main.js", "main.css"]
 async def async_setup(hass, config):
     """Set up this integration using yaml."""
-    url = f'/api/panel_custom/${DOMAIN}'
     for bundle_file_name in bundle_file_names:
+        url = f'/api/panel_custom/${DOMAIN}/${bundle_file_name}'
         location = hass.config.path(f'custom_components/${DOMAIN}/${bundle_file_name}')
         hass.http.register_static_path(url, location)
     hass.components.frontend.async_register_built_in_panel(
         component_name="custom",
         sidebar_title="Z2M",
         sidebar_icon="mdi:math-log",
-        frontend_url_path=DOMAIN,
+        frontend_url_path="z2m-la",
         config={
             "_panel_custom": {
                 "name": "z2m-light-admin",
