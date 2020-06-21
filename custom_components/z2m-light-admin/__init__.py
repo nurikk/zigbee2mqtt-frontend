7fbc77d8b5d3734694f8392f0ad3de08cd887639
nur <nur.php@gmail.com>
2020-06-21 12:04:52 +0800

    fix init

--- custom_components/z2m-light-admin/__init__.py
+++ custom_components/z2m-light-admin/__init__.py
@@ -2,12 +2,13 @@
 Hello world!
 """
 preffix = "z2m-light-admin"
-bundle_file_name = "test.js"
+bundle_file_names = ["main.js", "main.css"]
 async def async_setup(hass, config):
     """Set up this integration using yaml."""
     url = f'/api/panel_custom/${preffix}'
-    location = hass.config.path(f'custom_components/${preffix}/${bundle_file_name}')
-    hass.http.register_static_path(url, location)
+    for bundle_file_name in bundle_file_names:
+        location = hass.config.path(f'custom_components/${preffix}/${bundle_file_name}')
+        hass.http.register_static_path(url, location)
     hass.components.frontend.async_register_built_in_panel(
         component_name="custom",
         sidebar_title="Z2M",
@@ -15,7 +16,7 @@ async def async_setup(hass, config):
         frontend_url_path=preffix,
         config={
             "_panel_custom": {
-                "name": "ui-logs",
+                "name": "z2m-light-admin",
                 "embed_iframe": False,
                 "trust_external": True,
                 "js_url": url,
