d9b8b9ac06c991536658bdc4f3793895bc2d411f
nur <nur.php@gmail.com>
2020-09-06 06:14:52 +0800

    feat(mock): add networkmap mock response

--- ws.js
+++ ws.js
@@ -9,4 +9,17 @@ wss.on("connection", (ws) => {
     onConnect.forEach(msg => {
         ws.send(JSON.stringify(msg));
     });
+    ws.addEventListener("message", (message) => {
+        const msg = JSON.parse(message.data);
+        let response = [];
+        switch (msg.topic) {
+            case "bridge/request/networkmap":
+                response = require("./ws-messages/networkMapRequest.json");
+                break;
+            default:
+                break;
+        }
+        response.forEach(r => ws.send(JSON.stringify(r)));
+
+    })
 });
