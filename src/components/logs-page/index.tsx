7f6d959f2102b2e733572126a8278a44b2fbe807
nur <nur.php@gmail.com>
2020-09-13 12:25:44 +0800

    fix(logs page): add key

--- src/components/logs-page/index.tsx
+++ src/components/logs-page/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import { connect } from "unistore/react";
 import actions from "../../actions";
 import { GlobalState } from "../../store";
@@ -9,11 +9,12 @@ export class LogsPage extends Component<GlobalState, {}> {
     render() {
         const { logs } = this.props;
         return <div className="container-fluid h-100 overflow-auto pt-2">{
-            logs.map(l => <div><span className={cx({
-                'text-danger': l.level === 'error',
-                'text-warning': l.level === 'warning',
-                'text-info': l.level === 'info'
-            })} >{l.level.toUpperCase()}</span>:&nbsp;<code>{l.message}</code></div>)
+            logs.map((l, idx) => <div key={idx}>
+                <span className={cx({
+                    'text-danger': l.level === 'error',
+                    'text-warning': l.level === 'warning',
+                    'text-info': l.level === 'info'
+                })} >{l.level.toUpperCase()}</span>:&nbsp;<code>{l.message}</code></div>)
         }</div>
     }
 }
