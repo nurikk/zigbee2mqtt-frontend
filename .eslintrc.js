8444e33f3223228aadb2dfe5be4f66af55aaf8a9
nur <nur.php@gmail.com>
2020-09-15 05:21:24 +0800

    chore(eslint): explicit-function-return-type

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,6 @@ module.exports = {
     ],
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
     },
 };
