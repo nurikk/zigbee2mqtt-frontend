00e38ab36eb6f979c7e6e206bce7adbfc30281ff
nur <nur.php@gmail.com>
2020-10-12 06:58:25 +0800

    feat(device-image): add inverview state indicator

--- src/components/device-image/index.tsx
+++ src/components/device-image/index.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, ImgHTMLAttributes, useState } from "react";
 import genericDevice from "../../images/generic-zigbee-device.png";
 import { Device } from "../../types";
+import cx from "classnames";
 type DeviceImageProps = {
     device: Device;
     type?: "img" | "svg";
@@ -14,21 +15,27 @@ const AVALIABLE_GENERATORS = [
     genSlsDeviceImageUrlZ2M, genSlsDeviceImageUrlSLS, genericDeiviceImageFallback
 ]
 
-const DeviceImage: FunctionComponent<DeviceImageProps & ImgHTMLAttributes<HTMLImageElement | SVGImageElement>> = (props) => {
+const DeviceImage: FunctionComponent<DeviceImageProps & ImgHTMLAttributes<HTMLDivElement | SVGImageElement>> = (props) => {
     const [imageGenerators, setimageGenerators] = useState(AVALIABLE_GENERATORS);
-    const { device, type = "img", ...rest } = props;
+    const { device, type = "img", className, ...rest } = props;
     const src = imageGenerators.length ? imageGenerators[0](device) : false;
     const onImageError = () => {
         const newGenerators = [...imageGenerators];
         newGenerators.shift();
         setimageGenerators(newGenerators);
     };
+    const interviewSpinner = device.interviewing ? <i title="Interviewing" className="fa fa-spinner fa-spin position-absolute bottom-0 right-0" /> : null;
+    const unseccessfullInterview = !device.interviewing && !device.interview_completed ? <i title="Interview failed" className="fa fa-exclamation-triangle position-absolute top-0 right-0 text-danger" /> : null;
     switch (type) {
         case "svg":
             return src ? <image {...rest} onError={onImageError} href={src} /> : null;
         case "img":
         default:
-            return src ? <img {...rest} onError={onImageError} src={src} /> : null;
+            return src ? <div className={cx(className, "position-relative")} {...rest}>
+                <img  onError={onImageError} src={src} className={"position-relative"} />
+                {interviewSpinner}
+                {unseccessfullInterview}
+            </div> : null;
     }
 }
 export default DeviceImage;
