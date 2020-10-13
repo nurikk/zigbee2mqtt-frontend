c9de07a98b8d8b9434dd41f8433d8c9f278176db
nur <nur.php@gmail.com>
2020-10-14 06:22:09 +0800

    fix(color): convert colors to target namespace

--- src/components/color/index.tsx
+++ src/components/color/index.tsx
@@ -34,13 +34,32 @@ const toRGB = (source: AnyColor, brightness: number, sourceFormat: ColorFormat):
         return '#' + convertColors.hsv.hex([hue, saturation, scale(brightness, [1, 254], [0, 100])]);
       }
     default:
-      return '';
+      return '#FFFFFF';
   }
+}
+
+const rgbToTargetFormat = (source: string, brightness: number, targetFormat: ColorFormat): object => {
+  switch (targetFormat) {
+    case "color_hs":
+      const [h, s, v] = convertColors.hex.hsv(source);
+      return {h, s};
+
+
+    case "color_xy":
+      const [X, Y, Z] = convertColors.hex.xyz(source);
+      const x = X / (X + Y + Z);
+      const y = Y / (X + Y + Z);
+      return { x, y };
+
+    default:
+      return {hex: source};
+  }
+
 }
 const pridePallete = ['#FF0018', '#FFA52C', '#FFFF41', '#008018', '#0000F9', '#86007D'];
 const Color: FunctionComponent<ColorProps & Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'value'>> = (props) => {
 
-  const { onChange, name, value = {} as AnyColor, format, brightness = 254, steps = pridePallete, ...rest } = props;
+  const { onChange, value = {} as AnyColor, format, brightness = 254, name = 'color', steps = pridePallete, ...rest } = props;
   const [currentColor, setCurrentColor] = useState<string>(toRGB(value, brightness, format));
   useEffect(() => {
     setCurrentColor(toRGB(value, brightness, format))
@@ -55,7 +74,7 @@ const Color: FunctionComponent<ColorProps & Omit<InputHTMLAttributes<HTMLInputEl
           key={step}
           item={step}
           title={step}
-          onClick={(item) => onChange({ [name]: item })}>&nbsp;&nbsp;&nbsp;</Button>)
+          onClick={(item) => onChange({ [name]: rgbToTargetFormat(item, brightness, format) })}>&nbsp;&nbsp;&nbsp;</Button>)
       }
     </div>
     <input
@@ -63,7 +82,7 @@ const Color: FunctionComponent<ColorProps & Omit<InputHTMLAttributes<HTMLInputEl
       className="form-control form-control-color border-0 p-0"
       value={currentColor}
       onChange={e => {
-        onChange({ [name]: e.target.value })
+        onChange({ [name]: rgbToTargetFormat(e.target.value, brightness, format) })
       }}
       {...rest}
     />
