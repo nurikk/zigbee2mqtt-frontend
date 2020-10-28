8b04bfa7ff704badd012eab86dec2c83fde4f8db
nur <nur.php@gmail.com>
2020-10-29 07:36:25 +0800

    feat(error-boundry): implement error boundry

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,29 +1,45 @@
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import Button from "./button";
+import store from "../store";
+import { download } from "../utils";
 
 interface ErrorBoundaryState {
-    hasError: boolean;
+    error?: Error;
 }
-class ErrorBoundary extends React.Component<{}, ErrorBoundaryState> {
-    constructor(props) {
-        super(props);
-        this.state = { hasError: false };
+type ErrorBoundaryProps = RouteComponentProps<{}>;
+
+export default class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    state: Readonly<ErrorBoundaryState> = {};
+
+    static getDerivedStateFromError(error: Error) {
+        return { hasError: true, error };
     }
 
-    static getDerivedStateFromError(error: unknown) {
-        // Update state so the next render will show the fallback UI.
-        return { hasError: true };
+    donwloadState = () => {
+        download(JSON.stringify(store.getState(), null, 4), 'state.json');
     }
-
-
 
     render() {
-        if (this.state.hasError) {
-            // You can render any custom fallback UI
-            return <h1>Something went wrong.</h1>;
-        }
+        const { error } = this.state;
+        if (error) {
+            return <div className="container">
+                <h1 className="text-danger">Take screenshot of this page and attach it to github issue, additionally <Button<void> className="mt-2 btn btn-link" onClick={() => this.donwloadState()}>download state</Button> and attach to the issue</h1>
+                <h2>{error.name}</h2>
+                <h3>{error.message}</h3>
+                <pre>{error.stack}</pre>
+            </div>
 
+        }
         return this.props.children;
+    }
+
+    componentDidUpdate(prevProps: ErrorBoundaryProps) {
+        const { location: { pathname } } = this.props;
+        const { location: { pathname: prevPathname } } = prevProps;
+        if (prevPathname !== pathname) {
+            this.setState({ error: null });
+        }
     }
 }
 
-export default ErrorBoundary;
