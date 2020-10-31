2d114cec67ca7bbf20433b89051607b4e385e445
nur <nur.php@gmail.com>
2020-11-01 06:01:17 +0800

    feat(toggle): show unknown value state

--- src/components/toggle/index.tsx
+++ src/components/toggle/index.tsx
@@ -16,13 +16,16 @@ const Toggle: FunctionComponent<ToggleProps> = (props) => {
   const { onChange, value, label, valueOn, valueOff } = props;
   const [id, setId] = useState<string>(randomString(5));
   const onCheckboxChange = (e: ChangeEvent<HTMLInputElement>) => onChange(e.target.checked ? valueOn : valueOff);
+  const valueExists = value !== undefined && value !== null;
   return (
     <Fragment>
       <Button<unknown> className="btn btn-link pl-0" item={valueOff} onClick={(item) => onChange(item)}>{valueOff}</Button>
-      <div className="form-check form-switch d-inline-block">
+      {valueExists ? (
+        <div className="form-check form-switch d-inline-block">
         <input className="form-check-input" type="checkbox" id={id} checked={value === valueOn} onChange={onCheckboxChange} />
         {label ? <label className="form-check-label" htmlFor={id}>{label}</label> : null}
       </div>
+      ): <i className="fa fa-question" title="Current value unknown"></i> }
       <Button<unknown> className="btn btn-link" item={valueOn} onClick={(item) => onChange(item)}>{valueOn}</Button>
     </Fragment>
   )
