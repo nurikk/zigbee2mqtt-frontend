acdbcbf824ce8936f17f12a54d07a28eaba5adbd
nur <nur.php@gmail.com>
2020-11-02 06:00:40 +0800

    feat(ui): migrate device control to material-ui

--- src/components/device-control/index.tsx
+++ src/components/device-control/index.tsx
@@ -1,11 +1,25 @@
-import React, { ChangeEvent, Component } from "react";
-import Button from "../button";
+import React, { ChangeEvent, Component, Fragment } from "react";
+
 import { Device, DeviceState } from "../../types";
 import { connect } from "unistore/react";
 import actions, { OtaApi, DeviceApi } from "../../actions";
-import cx from "classnames";
-import Modal, { ModalHeader, ModalBody, ModalFooter } from "../modal";
+
 import { GlobalState } from "../../store";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import SystemUpdateAltIcon from '@material-ui/icons/SystemUpdateAlt';
+import SettingsIcon from '@material-ui/icons/Settings';
+import EditIcon from '@material-ui/icons/Edit';
+import IconButton from "@material-ui/core/IconButton";
+import Dialog from "@material-ui/core/Dialog";
+import SaveIcon from '@material-ui/icons/Save';
+import CloudIcon from '@material-ui/icons/Cloud';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+import { DialogTitle, DialogContent, TextField, DialogActions, Button, FormControlLabel, Checkbox, FormGroup } from "@material-ui/core";
+
+
 interface DeviceControlGroupProps {
     device: Device;
     state?: DeviceState;
@@ -71,98 +85,97 @@ export class DeviceControlGroup extends Component<DeviceControlGroupProps & Devi
         this.setState({ isDeviceRemovalModalOpened: !isDeviceRemovalModalOpened });
     }
 
-    onHassEntityIdChange = (e: ChangeEvent<HTMLInputElement>): void => {
+
+    onRenameParamsChange = (e: ChangeEvent<HTMLInputElement>): void => {
         const { renameParams } = this.state;
-        const { checked } = e.target;
-        renameParams.isHassRename = checked;
-        this.setState({ renameParams });
-    }
-    onFriendlyNameChange = (e: ChangeEvent<HTMLInputElement>): void => {
-        const { renameParams } = this.state;
-        const { value } = e.target;
-        renameParams.friendlyName = value;
-        this.setState({ renameParams });
+        const { checked, name, value, type } = e.target;
+        if (type === "checkbox") {
+            this.setState({ renameParams: { ...renameParams, [name]: checked } });
+        } else {
+            this.setState({ renameParams: { ...renameParams, [name]: value } });
+        }
     }
 
     onDeviceRemovalParamChange = (e: ChangeEvent<HTMLInputElement>): void => {
         const { removeParams } = this.state;
         const { checked, name } = e.target;
-        removeParams[name] = checked;
-        this.setState({ removeParams });
+        this.setState({ removeParams: { ...removeParams, [name]: checked } });
     }
-    renderRenameButton() {
-        const { bridgeInfo, device } = this.props;
+    renderRenameModal() {
+        const { bridgeInfo } = this.props;
         const { isRenameModalOpened, renameParams } = this.state;
         return (
-            <><Button<void> className="btn btn-secondary" onClick={this.toggleRenameModal} title="Rename device"><i className="fa fa-edit" /></Button>
-                <Modal isOpen={isRenameModalOpened}>
-                    <ModalHeader>
-                        <h3>Rename device</h3>
-                        <button type="button" className="close" aria-label="Close" onClick={this.toggleRenameModal}>
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </ModalHeader>
-                    <ModalBody>
-                        <div className="mb-3">
-                            <label htmlFor={`fn${device.ieee_address}`} className="form-label">Friendly name</label>
-                            <input id={`fn${device.ieee_address}`} onChange={this.onFriendlyNameChange} type="text" className="form-control" value={renameParams.friendlyName} />
-                        </div>
-                        {bridgeInfo.config.homeassistant ? (
-                            <div className="form-check form-switch">
-                                <input className="form-check-input" checked={renameParams.isHassRename} type="checkbox" id={`hass${device.ieee_address}`} onChange={this.onHassEntityIdChange} />
-                                <label className="form-check-label" htmlFor={`hass${device.ieee_address}`}>Update Home Assistant entity ID</label>
-                            </div>
-                        ) : null}
-                    </ModalBody>
-                    <ModalFooter>
-                        <button type="button" className="btn btn-secondary" onClick={this.toggleRenameModal}>Close</button>
-                        <button type="button" className="btn btn-primary" onClick={this.onRenameClick}>Save changes</button>
-                    </ModalFooter>
-                </Modal></>
+            <Dialog open={isRenameModalOpened} onClose={this.toggleRenameModal} aria-labelledby="form-dialog-title">
+                <DialogTitle>Rename device</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        value={renameParams.friendlyName}
+                        onChange={this.onRenameParamsChange}
+                        autoFocus
+                        margin="dense"
+                        label="Friendly name"
+                        type="text"
+                        fullWidth
+                        name="friendlyName"
+                    />
+                    {bridgeInfo.config.homeassistant ? (
+                        <FormControlLabel
+                            control={<Checkbox name="isHassRename" checked={renameParams.isHassRename} onChange={this.onRenameParamsChange} />}
+                            label="Update Home Assistant entity ID"
+                        />
+                    ) : null}
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.toggleRenameModal} color="primary">Close</Button>
+                    <Button onClick={this.onRenameClick} color="secondary" variant="contained" startIcon={<SaveIcon />}>Save changes</Button>
+                </DialogActions>
+            </Dialog>
         )
     }
-    renderDeviceRemovalButton() {
-        const { device } = this.props;
+    renderDeviceRemovalModal() {
         const { isDeviceRemovalModalOpened, removeParams } = this.state;
         return (
-            <><Modal isOpen={isDeviceRemovalModalOpened}>
-                <ModalHeader>
-                    <h3>Remove device</h3>
-                    <button type="button" className="close" aria-label="Close" onClick={this.toggleDeviceRemovalModal}><span aria-hidden="true">&times;</span></button>
-                </ModalHeader>
-                <ModalBody>
-                    <div className="form-check form-switch">
-                        <input className="form-check-input" name="force" checked={removeParams.force} type="checkbox" id={`force${device.ieee_address}`} onChange={this.onDeviceRemovalParamChange} />
-                        <label className="form-check-label" htmlFor={`force${device.ieee_address}`}>Force remove</label>
-                    </div>
-                    <div className="form-check form-switch">
-                        <input className="form-check-input" name="block" checked={removeParams.block} type="checkbox" id={`block${device.ieee_address}`} onChange={this.onDeviceRemovalParamChange} />
-                        <label className="form-check-label" htmlFor={`block${device.ieee_address}`}>Block from joining again</label>
-                    </div>
-                </ModalBody>
-                <ModalFooter>
-                    <button type="button" className="btn btn-secondary" onClick={this.toggleDeviceRemovalModal}>Close</button>
-                    <button type="button" className="btn btn-danger" onClick={this.onRemoveClick}>Delete</button>
-                </ModalFooter>
-            </Modal>
-                <button onClick={this.toggleDeviceRemovalModal} className="btn btn-danger" title="Remove device"><i className={cx("fa", "fa-trash")} /></button></>
+            <Dialog open={isDeviceRemovalModalOpened} onClose={this.toggleDeviceRemovalModal} aria-labelledby="form-dialog-title">
+                <DialogTitle>Remove device</DialogTitle>
+                <DialogContent>
+                    <FormGroup row>
+                        <FormControlLabel
+                            control={<Checkbox checked={removeParams.force} name="force" onChange={this.onDeviceRemovalParamChange} />}
+                            label="Force remove"
+                        />
+                    </FormGroup>
+                    <FormGroup row>
+                        <FormControlLabel
+                            control={<Checkbox checked={removeParams.block} name="block" onChange={this.onDeviceRemovalParamChange} />}
+                            label="Block from joining again"
+                        />
+                    </FormGroup>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.toggleDeviceRemovalModal} color="primary">Close</Button>
+                    <Button onClick={this.onRenameClick} variant="contained" color="secondary" startIcon={<DeleteIcon />}>Delete</Button>
+                </DialogActions>
+            </Dialog>
         )
     }
     render() {
         const { device, configureDevice, checkOTA, updateOTA, state } = this.props;
         return (
-            <div className="btn-group btn-group-sm" role="group">
-                {this.renderRenameButton()}
-                <Button<string> className="btn btn-secondary" onClick={configureDevice} item={device.friendly_name} title="Reconfigure" promt><i className={cx("fa", "fa-cogs")} /></Button>
-                {
-                    state?.update?.state === "available" ?
-                        <Button<string> className="btn btn-secondary" onClick={updateOTA} item={device.friendly_name} title="Update OTA" promt><i className={cx("fa", "fa-cloud-download-alt")} /></Button>
-                        : <Button<string> className="btn btn-secondary" onClick={checkOTA} item={device.friendly_name} title="Check OTA"><i className={cx("fa", "fa-cloud")} /></Button>
-                }
-                {this.renderDeviceRemovalButton()}
+            <Fragment>
+                {this.renderRenameModal()}
+                {this.renderDeviceRemovalModal()}
+                <ButtonGroup  size="small" variant="contained">
+                    <Button onClick={this.toggleRenameModal} title="Rename device"><EditIcon /></Button>
+                    <Button onClick={() => configureDevice(device.friendly_name)} title="Reconfigure"><SettingsIcon /></Button>
+                    {
+                        state?.update?.state === "available" ?
+                            <Button onClick={() => updateOTA(device.friendly_name)} title="Update OTA"><CloudDownloadIcon /></Button>
+                            : <Button onClick={() => checkOTA(device.friendly_name)} title="Check OTA"><CloudIcon /></Button>
+                    }
+                    <Button color="secondary" onClick={this.toggleDeviceRemovalModal} title="Remove device"><DeleteIcon /></Button>
+                </ButtonGroup>
+            </Fragment>
 
-
-            </div>
         );
     }
 }
