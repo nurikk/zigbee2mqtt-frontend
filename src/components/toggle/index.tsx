db2961012722878e7da3d8dca291e5d2b80a71c5
nur <nur.php@gmail.com>
2021-02-06 14:28:25 +0800

    refactor(toggle): do DRY

--- src/components/toggle/index.tsx
+++ src/components/toggle/index.tsx
@@ -13,24 +13,36 @@ type ToggleProps = {
     label?: string;
     minimal?: boolean;
 }
-
+type ControlButtonProps = {
+    value: unknown;
+    onClick(value: unknown): void;
+    name: string;
+}
+const ControlButton: FunctionComponent<ControlButtonProps> = (props) => {
+    const { value, onClick, name } = props;
+    return <Button<unknown> className="btn btn-link ps-0" item={value} onClick={onClick}>
+        <DisplayValue value={value} name={name} />
+    </Button>
+}
 
 const Toggle: FunctionComponent<ToggleProps> = (props) => {
     const { onChange, value, label, valueOn, valueOff, minimal, name } = props;
     const [id, setId] = useState<string>(randomString(5));
     const onCheckboxChange = (e: ChangeEvent<HTMLInputElement>) => onChange(e.target.checked ? valueOn : valueOff);
     const valueExists = value !== undefined && value !== null;
-    const showOnOffButtons =  !minimal || (minimal && !valueExists);
+    const showOnOffButtons = !minimal || (minimal && !valueExists);
     return (
         <Fragment>
-            {showOnOffButtons && <Button<unknown> className="btn btn-link ps-0" item={valueOff} onClick={(item) => onChange(item)}><DisplayValue value={valueOff} name={name}/></Button>}
+
+            {showOnOffButtons && <ControlButton value={valueOff} name={name} onClick={onChange} />}
             {valueExists ? (
                 <div className="form-check form-switch d-inline-block">
                     <input className="form-check-input" type="checkbox" id={id} checked={value === valueOn} onChange={onCheckboxChange} />
                     {label ? <label className="form-check-label" htmlFor={id}>{label}</label> : null}
                 </div>
-            ) : <i  className="fa fa-question" title="Current value unknown"></i>}
-            {showOnOffButtons && <Button<unknown> className="btn btn-link" item={valueOn} onClick={(item) => onChange(item)}><DisplayValue value={valueOn} name={name} /></Button>}
+            ) : <i className="fa fa-question" title="Current value unknown"></i>}
+            {showOnOffButtons && <ControlButton value={valueOn} name={name} onClick={onChange} />}
+
         </Fragment>
     )
 }
