f3b3c947e95477068a6e6c2138a7c5c32fec41f2
xyzroe <me@kogan.in.ua>
2021-04-09 15:51:27 +0300

    Update index.tsx

--- src/components/dashboard-page/index.tsx
+++ src/components/dashboard-page/index.tsx
@@ -19,7 +19,7 @@ import groupBy from "lodash/groupBy";
 type Props = Pick<GlobalState, 'devices' | 'deviceStates' | 'bridgeInfo'> & StateApi;
 
 const genericRendererIgnoredNames = ['linkquality', 'battery', 'battery_low', 'illuminance_lux', 'color_temp_startup', 'voltage', 'strength', 'color_options'];
-const whitelistFeatureNames = ['state', 'brightness', 'color_temp', 'mode', 'sound'];
+const whitelistFeatureNames = ['state', 'brightness', 'color_temp', 'mode', 'sound', 'occupancy', 'tamper'];
 const whitelistFeatureTypes = ['light'];
 const nullish = ['', null, undefined];
 
