847951231692acb9b4a6c57b71f081b416ffcf9e
nur <nur.php@gmail.com>
2021-05-09 00:55:51 +0800

    fix(i18n): fix default language

--- src/i18n/LocalePicker.tsx
+++ src/i18n/LocalePicker.tsx
@@ -26,11 +26,11 @@ export default function LocalePicker(): JSX.Element {
         <img src={localesMap[language]} alt={t(language)} width="20" className="align-middle me-1" />
         <span className="align-middle">{t(language)}</span>
     </a>));
-
+    const currentLanguage = i18n.language.split('-')[0];
 
     return (<li className="nav-item dropdown">
-        <a className={cx("nav-flag dropdown-toggle", { show: isComponentVisible })} href="#" onClick={() => setIsComponentVisible(!isComponentVisible)} aria-expanded="false">
-            <img src={localesMap[i18n.language]} alt={t(i18n.language)} />
+        <a className={cx("nav-flag dropdown-toggle", { show: isComponentVisible })} href="#" onClick={() => setIsComponentVisible(!isComponentVisible)} >
+            <img src={localesMap[currentLanguage]} alt={t(currentLanguage)} />
         </a>
         <div ref={ref as RefObject<HTMLDivElement>} className={cx("dropdown-menu dropdown-menu-end", { show: isComponentVisible })}>
             {locales}
