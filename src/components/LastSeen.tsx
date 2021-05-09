4ebc23fcc3327dfc36a5c8d34d9ce53ba169830e
nur <nur.php@gmail.com>
2021-05-09 10:23:09 +0800

    feat(i18n): translate lastSeen

--- src/components/LastSeen.tsx
+++ src/components/LastSeen.tsx
@@ -3,16 +3,18 @@ import { DeviceState } from "../types";
 import { format } from 'timeago.js';
 import { lastSeen } from "../utils";
 import { LastSeenType } from "./zigbee";
+import { useTranslation } from "react-i18next";
 
 type LastSeenProps = {
     state: DeviceState;
     lastSeenType: LastSeenType;
 }
 export function LastSeen(props: LastSeenProps): JSX.Element {
+    const { i18n } = useTranslation();
     const { state, lastSeenType } = props;
     const lastSeenDate = lastSeen(state, lastSeenType);
     if (lastSeenDate) {
-        return <>{format(lastSeenDate)}</>
+        return <>{format(lastSeenDate, i18n.language)}</>
     } else {
         return <>N/A</>;
     }
--- src/i18n/LocalePicker.tsx
+++ src/i18n/LocalePicker.tsx
@@ -1,13 +1,20 @@
 
 import React, { RefObject } from 'react';
-
 import { useTranslation } from 'react-i18next';
+import { register } from "timeago.js"
 import useComponentVisible from '../hooks/useComponentVisible';
 import cx from "classnames";
 import { Resource } from 'i18next';
 import en from "./flags/us.png";
 import fr from "./flags/fr.png";
 import pl from "./flags/pl.png";
+
+import timePl from "timeago.js/lib/lang/pl";
+import timeFR from "timeago.js/lib/lang/fr";
+
+
+register("pl", timePl);
+register("fr", timeFR);
 
 const localesMap = {
     en, fr, pl
