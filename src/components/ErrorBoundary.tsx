3eeceaa8fa0156b502e5382e2b027ea234191076
nur <nur.php@gmail.com>
2021-05-09 21:42:23 +0800

    fix(error-boundry): update steps

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -51,6 +51,7 @@ export default class ErrorBoundary extends React.Component<ErrorBoundaryProps, E
 
                     <li className="fs-1 lh-lg"><a target="_blank" rel="noopener noreferrer" href={githubUrl}>Raise a github issue</a>, attach previously donwloaded file</li>
                     <li className="fs-1 lh-lg">Take a screenshot of this page and attach to the issue</li>
+                    <li className="fs-1 lh-lg">In the github issue write detailed description for the issue, how this happened? Steps to reproduce</li>
                 </ol>
                 <div>
                     <div>{error.name}</div>
