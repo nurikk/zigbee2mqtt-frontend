66bb1db057b2761015020f5eb867b6a90287dd2b
nur <nur.php@gmail.com>
2021-05-10 21:57:49 +0800

    fix(i18): add DE locale, small refactoring

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,14 +1,32 @@
 import i18n, { ResourceLanguage } from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { register } from "timeago.js"
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+
+
 import enTranslations from './locales/en.json';
 import frTranslations from './locales/fr.json';
 import plTranslations from './locales/pl.json';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import deTranslations from './locales/de.json';
+
+
+import timePl from "timeago.js/lib/lang/pl";
+import timeFR from "timeago.js/lib/lang/fr";
+import timeDe from "timeago.js/lib/lang/de";
+
+
+register("pl", timePl);
+register("fr", timeFR);
+register("de", timeDe);
+
+
 
 export const resources = {
     en: enTranslations as ResourceLanguage,
     fr: frTranslations as ResourceLanguage,
-    pl: plTranslations as ResourceLanguage
+    pl: plTranslations as ResourceLanguage,
+    de: deTranslations as ResourceLanguage
 } as const;
 
 i18n
