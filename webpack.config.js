71183f70ffad474807f0760606e0cf5222e92fa8
nur <nur.php@gmail.com>
2021-05-11 09:29:43 +0800

    refactor(webpack): refactor webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,115 @@ const getPlugins = (production) => {
     return plugins;
 };
 
+const config = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve("./dist"),
+    },
+    target: "web",
+    devtool: "source-map",
+    optimization: {
+        usedExports: true,
+        runtimeChunk: "single",
+        splitChunks: {
+            cacheGroups: {
+                defaultVendors: {
+                    test: /node_modules/,
+                    name: "scripts/vendor",
+                    chunks: "all",
+                    enforce: true,
+                },
+            },
+        },
+    },
+    resolve: {
+        // mainFields: ["module", "main"],
+        extensions: [".ts", ".tsx", ".js", ".html", ".txt"]
+    },
+    module: {
+        rules: [{
+                test: /\.txt$/i,
+                type: 'asset/source'
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.tsx?$/,
+                use: [{
+                    loader: "ts-loader",
+                }, ],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
 
+                    "@teamsupercell/typings-for-css-modules-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                        },
+                    },
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                include: /node_modules/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+
+            {
+                test: /\.scss$/,
+                include: [/\.global\./, /node_modules/],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+                sideEffects: true,
+            },
+            {
+                // Preprocess our own .css files
+                // This is the place to add your own loaders (e.g. sass/less etc.)
+                // for a list of loaders, see https://webpack.js.org/loaders/#styling
+                test: /\.scss$/,
+                exclude: [/\.global\./, /node_modules/],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    '@teamsupercell/typings-for-css-modules-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            modules: true,
+                            importLoaders: 1,
+                        },
+                    },
+                    'sass-loader',
+                ],
+                sideEffects: true,
+            },
+        ],
+    },
+    devServer: {
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+        contentBase: "./dist",
+        compress: true,
+        host: "0.0.0.0",
+        port: 3030,
+        proxy: {
+            "/api": {
+                target: proxyTo,
+                ws: true,
+            },
+        },
+    },
+    stats: "errors-only",
+    externals: {}
+};
 module.exports = (env, args) => {
     let production = false;
 
@@ -62,114 +170,5 @@ module.exports = (env, args) => {
     }
     const plugins = getPlugins(production);
 
-    return {
-        entry: "./src/index.tsx",
-        output: {
-            filename: "[name].[contenthash].js",
-            path: path.resolve("./dist"),
-        },
-        target: "web",
-        devtool: "source-map",
-        optimization: {
-            usedExports: true,
-            runtimeChunk: "single",
-            splitChunks: {
-                cacheGroups: {
-                    defaultVendors: {
-                        test: /node_modules/,
-                        name: "scripts/vendor",
-                        chunks: "all",
-                        enforce: true,
-                    },
-                },
-            },
-        },
-        resolve: {
-            // mainFields: ["module", "main"],
-            extensions: [".ts", ".tsx", ".js", ".html", ".txt"]
-        },
-        module: {
-            rules: [{
-                    test: /\.txt$/i,
-                    type: 'asset/source'
-                },
-                {
-                    test: /\.(png|jpe?g|gif)$/i,
-                    type: 'asset/resource',
-                },
-                {
-                    test: /\.tsx?$/,
-                    use: [{
-                        loader: "ts-loader",
-                    }, ],
-                },
-                {
-                    test: /\.css$/i,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-
-                        "@teamsupercell/typings-for-css-modules-loader",
-                        {
-                            loader: "css-loader",
-                            options: {
-                                modules: true,
-                            },
-                        },
-                    ],
-                    exclude: /node_modules/,
-                },
-                {
-                    test: /\.css$/i,
-                    include: /node_modules/,
-                    use: [MiniCssExtractPlugin.loader, "css-loader"],
-                },
-
-                {
-                    test: /\.scss$/,
-                    include: [/\.global\./, /node_modules/],
-                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-                    sideEffects: true,
-                },
-                {
-                    // Preprocess our own .css files
-                    // This is the place to add your own loaders (e.g. sass/less etc.)
-                    // for a list of loaders, see https://webpack.js.org/loaders/#styling
-                    test: /\.scss$/,
-                    exclude: [/\.global\./, /node_modules/],
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        '@teamsupercell/typings-for-css-modules-loader',
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: true,
-                                modules: true,
-                                importLoaders: 1,
-                            },
-                        },
-                        'sass-loader',
-                    ],
-                    sideEffects: true,
-                },
-            ],
-        },
-        devServer: {
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-            },
-            contentBase: "./dist",
-            compress: true,
-            host: "0.0.0.0",
-            port: 3030,
-            proxy: {
-                "/api": {
-                    target: proxyTo,
-                    ws: true,
-                },
-            },
-        },
-        plugins,
-        stats: "errors-only",
-        externals: {}
-    };
+    return {...config, plugins}
 };
