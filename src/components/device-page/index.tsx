c50bb8c2acd13318611005a07f4e6e39b710de90
nur <nur.php@gmail.com>
2021-05-16 16:41:56 +0800

    fix(device-settings): fix value ovvirides, deprecate device-specific settings

--- src/components/device-page/index.tsx
+++ src/components/device-page/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink, Redirect, RouteComponentProps, withRouter } from "react-router-dom";
 import { connect } from "unistore/react";
 import actions from "../../actions/actions";
@@ -16,15 +16,6 @@ import DevConsole from "./dev-console";
 import { DeviceApi } from "../../actions/DeviceApi";
 import { WithTranslation, withTranslation } from "react-i18next";
 
-
-type UrlParams = {
-    dev: string;
-    tab?: TabName;
-};
-type DevicePageProps = RouteComponentProps<UrlParams>;
-
-
-type TabName = "info" | "bind" | "state" | "exposes" | "clusters" | "reporting" | "settings" | "settings-specific" | "dev-console";
 const getDeviceLinks = (dev: string) => ([
     {
         translationKey: 'about',
@@ -64,72 +55,81 @@ const getDeviceLinks = (dev: string) => ([
     },
 
 ]);
-export class DevicePage extends Component<DevicePageProps & GlobalState & DeviceApi & WithTranslation<"devicePage">, {}> {
-    renderContent(): JSX.Element {
-        const { match, devices, logs } = this.props;
-        const { readDeviceAttributes, writeDeviceAttributes } = this.props;
-        const { tab, dev } = match.params;
-        const device = devices[dev];
+type TabName = "info" | "bind" | "state" | "exposes" | "clusters" | "reporting" | "settings" | "settings-specific" | "dev-console";
+type UrlParams = {
+    dev: string;
+    tab?: TabName;
+};
+type DevicePageProps = RouteComponentProps<UrlParams> & GlobalState & DeviceApi & WithTranslation<"devicePage">;
 
-        switch (tab) {
-            case "info":
-                return <DeviceInfo device={device} />;
-            case "bind":
-                return <Bind device={device} />;
-            case "state":
-                return <States device={device} />;
-            case "exposes":
-                return <ConnectedDeviceExposes device={device} />;
-            case "clusters":
-                return <Clusters device={device} />
-            case "reporting":
-                return <Reporting device={device} />
-            case "settings":
-                return <DeviceSettings device={device} type="generic" />
-            case "settings-specific":
-                return <DeviceSettings device={device} type="specific" />
-            case "dev-console":
-                return <DevConsole
-                    device={device}
-                    logs={logs}
-                    readDeviceAttributes={readDeviceAttributes}
-                    writeDeviceAttributes={writeDeviceAttributes}
-                />
-            default:
-                return <Redirect to={`/device/${dev}/info`} />;
-        }
+function ContentRenderer(props: DevicePageProps): JSX.Element {
+    const { match, devices, logs } = props;
+    const { readDeviceAttributes, writeDeviceAttributes, setDeviceOptions, bridgeInfo } = props;
+    const { tab, dev } = match.params;
+    const device = devices[dev];
 
+    switch (tab) {
+        case "info":
+            return <DeviceInfo device={device} />;
+        case "bind":
+            return <Bind device={device} />;
+        case "state":
+            return <States device={device} />;
+        case "exposes":
+            return <ConnectedDeviceExposes device={device} />;
+        case "clusters":
+            return <Clusters device={device} />
+        case "reporting":
+            return <Reporting device={device} />
+        case "settings":
+            return <DeviceSettings
+                device={device}
+                setDeviceOptions={setDeviceOptions}
+                bridgeInfo={bridgeInfo}
+            />
+        case "settings-specific":
+            return <div>Under construction</div>
+        case "dev-console":
+            return <DevConsole
+                device={device}
+                logs={logs}
+                readDeviceAttributes={readDeviceAttributes}
+                writeDeviceAttributes={writeDeviceAttributes}
+            />
+        default:
+            return <Redirect to={`/device/${dev}/info`} />;
     }
-    render(): JSX.Element {
-        const { devices, match, t } = this.props;
-        const { dev } = match.params;
-        const device = devices[dev];
-        if (!device) {
-            return <div className="h-100 d-flex justify-content-center align-items-center">{t('unknown_device')}</div>
-        }
-        const links = getDeviceLinks(dev);
 
-        return (<>
-            <h1 className="h3">{device.friendly_name}</h1>
+}
 
-            <div className="tab">
-                <ul className="nav nav-tabs">
-                    {links.map(link => <li key={link.translationKey} className="nav-item">
-                        <NavLink activeClassName="active" className={`nav-link ${styles['small-nav']}`} to={link.url}>{t(link.translationKey)}</NavLink>
-                    </li>)}
-                </ul>
-                <div className="tab-content">
-                    <div className="tab-pane fade show active">
-                        {this.renderContent()}
-                    </div>
+export function DevicePage(props: DevicePageProps): JSX.Element {
+    const { devices, match, t } = props;
+    const { dev } = match.params;
+    const device = devices[dev];
+    if (!device) {
+        return <div className="h-100 d-flex justify-content-center align-items-center">{t('unknown_device')}</div>
+    }
+    const links = getDeviceLinks(dev);
+
+    return (<>
+        <h1 className="h3">{device.friendly_name}</h1>
+
+        <div className="tab">
+            <ul className="nav nav-tabs">
+                {links.map(link => <li key={link.translationKey} className="nav-item">
+                    <NavLink activeClassName="active" className={`nav-link ${styles['small-nav']}`} to={link.url}>{t(link.translationKey)}</NavLink>
+                </li>)}
+            </ul>
+            <div className="tab-content">
+                <div className="tab-pane fade show active">
+                    <ContentRenderer {...props} />
                 </div>
             </div>
+        </div>
+    </>);
 
-        </>);
-
-    }
 }
 const devicePageWithRouter = withRouter(DevicePage);
-const mappedProps = ["devices", "deviceStates", "logs"];
+const mappedProps = ["devices", "deviceStates", "logs", "bridgeInfo"];
 const ConnectedDevicePage = withTranslation("devicePage")(connect<unknown, unknown, GlobalState, unknown>(mappedProps, actions)(devicePageWithRouter));
 export default ConnectedDevicePage;
--- src/components/device-page/settings.tsx
+++ src/components/device-page/settings.tsx
@@ -1,21 +1,16 @@
-/* eslint-disable react/display-name */
 import React, { Component, ReactNode } from "react";
-import { Device, KVP } from "../../types";
-import { connect } from "unistore/react";
-import { GlobalState } from "../../store";
-import actions from "../../actions/actions";
+import { BridgeInfo, Device, KVP } from "../../types";
 import { DeviceApi } from "../../actions/DeviceApi";
 import Form from '@rjsf/bootstrap-4';
 import { JSONSchema7 } from "json-schema"
 import { ISubmitEvent, UiSchema } from "@rjsf/core";
-import deviceSpecificSchema from "./deviceSpecificSchema.json";
-import { ModelLink } from "../vendor-links/verndor-links";
-import customFields from "./../../i18n/rjsf-translation-fields";
-type SettingsType = "generic" | "specific";
 
-type DeviceSettingsProps = {
+import customFields from "./../../i18n/rjsf-translation-fields";
+import merge from "lodash/merge";
+
+interface DeviceSettingsProps extends Pick<DeviceApi, "setDeviceOptions"> {
     device: Device;
-    type: SettingsType;
+    bridgeInfo: BridgeInfo;
 }
 
 type ParamValue = {
@@ -28,93 +23,53 @@ const genericUiSchema: UiSchema = {
     "ui:order": ["friendly_name", "retain", "retention", "qos", "filtered_attributes", "*"]
 };
 
-const toType = (obj: unknown): string => ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
 type DeviceSettingsState = {
     newSetting: ParamValue;
+    updatedDeviceConfig: KVP | KVP[];
 }
-export class DeviceSettings extends Component<DeviceSettingsProps & GlobalState & DeviceApi, DeviceSettingsState> {
+export default class DeviceSettings extends Component<DeviceSettingsProps, DeviceSettingsState> {
     state = {
         newSetting: {
             key: "",
             value: "",
             type: ""
-        } as ParamValue
+        } as ParamValue,
+
+        updatedDeviceConfig: {}
     }
     getGenericDeviceSettingsSchema(): JSONSchema7 {
         const { bridgeInfo: { config_schema: configSchema } } = this.props;
         return (configSchema.definitions?.device ?? { properties: {} }) as JSONSchema7;
     }
-    getDeviceConfig(): Record<string, unknown> {
+    getDeviceConfig(): KVP | KVP[] {
         const { bridgeInfo: { config }, device } = this.props;
-        return { ...config?.device_options, ...config?.devices[device.ieee_address] };
+        const { updatedDeviceConfig } = this.state;
+        return merge({}, updatedDeviceConfig, config?.device_options, config?.devices[device.ieee_address]);
+    }
+    onFormChange = (params: ISubmitEvent<KVP | KVP[]>): void => {
+        const { formData } = params;
+        this.setState({ updatedDeviceConfig: formData });
     }
     updateConfig = (params: ISubmitEvent<KVP | KVP[]>): void => {
         const { formData } = params;
-        const { setDeviceOptions, type, device } = this.props;
-        const genericPropNames = Object.keys(this.getGenericDeviceSettingsSchema().properties as KVP);
-        if (type === "generic") {
-            setDeviceOptions(device.ieee_address, formData as Record<string, unknown>);
-        } else {
-
-            const params = (formData as ParamValue[]).reduce((result, curr) => {
-                if (curr.key) {
-                    result[curr.key] = curr.value;
-                }
-                return result;
-            }, {});
-            Object.entries(this.getDeviceConfig())
-                .filter(([key]) => !genericPropNames.includes(key))
-                .forEach(([key]) => {
-                    const updatedProp = (formData as KVP[]).find(f => f.key === key) as KVP;
-                    if (updatedProp !== undefined) {
-                        params[key] = updatedProp.value;
-                    } else {
-                        params[key] = undefined;
-                    }
-                });
-            setDeviceOptions(device.ieee_address, params);
-        }
+        const { setDeviceOptions, device } = this.props;
+        setDeviceOptions(device.ieee_address, formData as Record<string, unknown>);
     }
 
     getSchemaAndConfig(): { schema: JSONSchema7, data: KVP | KVP[], uiSchema: UiSchema } {
-        const { newSetting } = this.state;
-        const { type } = this.props;
         const genericDeviceSettingsSchema = this.getGenericDeviceSettingsSchema();
         const deviceConfig = this.getDeviceConfig();
-        if (type === "generic") {
-            return { schema: genericDeviceSettingsSchema, data: deviceConfig, uiSchema: genericUiSchema };
-        } else {
-            const genericPropNames = Object.keys(genericDeviceSettingsSchema.properties as KVP);
-            const filteredDeviceConfig = Object.entries(deviceConfig)
-                .filter(([key]) => !genericPropNames.includes(key))
-                .map(([key, value]) => {
-                    const valueType = toType(value);
-                    return { value, valueType, key } as unknown as ParamValue;
-                })
-            return { schema: deviceSpecificSchema as JSONSchema7, data: [newSetting, ...filteredDeviceConfig], uiSchema: {} }
-        }
+        return { schema: genericDeviceSettingsSchema, data: deviceConfig, uiSchema: genericUiSchema };
     }
-    renderHelp(): ReactNode {
-        const { type, device } = this.props;
-        if (type === "specific") {
-            return <h2>Get possible device specific parameters on <ModelLink device={device} anchor="device-type-specific-configuration" /></h2>
-        }
-    }
+
     render(): ReactNode {
         const { schema, data, uiSchema } = this.getSchemaAndConfig();
-        return <>
-            {this.renderHelp()}
-            <Form schema={schema as JSONSchema7}
-                formData={data}
-                onSubmit={this.updateConfig}
-                uiSchema={uiSchema}
-                fields={customFields}
-            />
-        </>;
+        return <Form schema={schema as JSONSchema7}
+            formData={data}
+            onSubmit={this.updateConfig}
+            onChange={this.onFormChange}
+            uiSchema={uiSchema}
+            fields={customFields}
+        />;
     }
 }
-
-const mappedProps = ["bridgeInfo"];
-
-const ConnectedDeviceSettingsPage = connect<DeviceSettingsProps, unknown, GlobalState, DeviceApi>(mappedProps, actions)(DeviceSettings);
-export default ConnectedDeviceSettingsPage;
