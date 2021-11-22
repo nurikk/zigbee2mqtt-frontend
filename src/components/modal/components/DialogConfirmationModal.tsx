31ff0e7b270123920b7b9ac497adf97caa9b6f5e
nurikk <ainur@yojee.com>
2021-11-22 15:04:11 +0800

    fix(modals): introduce global modal dialogs

---
+++ src/components/modal/components/DialogConfirmationModal.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useGlobalModalContext } from "../GlobalModal";
+import Modal, { ModalBody, ModalFooter, ModalHeader } from "../Modal";
+
+type DialogConfirmationModalProps = {
+    onConfirmHandler(): void;
+}
+
+export const DialogConfirmationModal = (props: DialogConfirmationModalProps) => {
+    const { onConfirmHandler } = props;
+    const { t } = useTranslation("common");
+    const { hideModal } = useGlobalModalContext();
+
+    return (
+        <Modal isOpen={true}>
+            <ModalHeader>
+                <h3>{t('confirmation')}</h3>
+            </ModalHeader>
+            <ModalBody>
+                {t('dialog_confirmation_prompt')}
+            </ModalBody>
+            <ModalFooter>
+                <button type="button" className="btn btn-secondary" onClick={hideModal}>{t('common:close')}</button>
+                <button type="button" className="btn btn-primary" onClick={onConfirmHandler}>{t('common:ok')}</button>
+            </ModalFooter>
+        </Modal>
+    );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -37,6 +37,7 @@ import StateNotifier from "./components/state-notifier/StateNotifier";
 import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 import i18n from "./i18n";
 import { I18nextProvider } from 'react-i18next';
+import { GlobalModal } from './components/modal/GlobalModal';
 
 
 
@@ -56,32 +57,34 @@ const Main = () => {
     return <Suspense fallback="loading">
         <I18nextProvider i18n={i18n}>
             <Provider store={store}>
-                <ThemeSwitcherProvider themeMap={themes} defaultTheme={theme}>
-                    <HashRouter>
-                        <StateNotifier />
-                        <div className="main">
-                            <NavBar />
-                            <main className="content p-0 p-sm-3">
-                                <div className="container-fluid p-0 h-100">
-                                    <Switch>
-                                        <Route path="/ota" render={(props) => <ErrorBoundary {...props}><OtaPage /></ErrorBoundary>} />
-                                        <Route path="/map" render={(props) => <ErrorBoundary {...props}><ConnectedMap /></ErrorBoundary>} />
-                                        <Route path="/device/:dev/:tab?" render={(props) => <ErrorBoundary {...props}><ConnectedDevicePage /></ErrorBoundary>} />
-                                        <Route path="/settings/:tab?" render={(props) => <ErrorBoundary {...props}><ConnectedSettingsPage /></ErrorBoundary>} />
-                                        <Route path="/groups" render={(props) => <ErrorBoundary {...props}><ConnectedGroupsPage /></ErrorBoundary>} />
-                                        <Route path="/group/:groupId?" render={(props) => <ErrorBoundary {...props}><GroupPage /></ErrorBoundary>} />
+                <GlobalModal>
+                    <ThemeSwitcherProvider themeMap={themes} defaultTheme={theme}>
+                        <HashRouter>
+                            <StateNotifier />
+                            <div className="main">
+                                <NavBar />
+                                <main className="content p-0 p-sm-3">
+                                    <div className="container-fluid p-0 h-100">
+                                        <Switch>
+                                            <Route path="/ota" render={(props) => <ErrorBoundary {...props}><OtaPage /></ErrorBoundary>} />
+                                            <Route path="/map" render={(props) => <ErrorBoundary {...props}><ConnectedMap /></ErrorBoundary>} />
+                                            <Route path="/device/:dev/:tab?" render={(props) => <ErrorBoundary {...props}><ConnectedDevicePage /></ErrorBoundary>} />
+                                            <Route path="/settings/:tab?" render={(props) => <ErrorBoundary {...props}><ConnectedSettingsPage /></ErrorBoundary>} />
+                                            <Route path="/groups" render={(props) => <ErrorBoundary {...props}><ConnectedGroupsPage /></ErrorBoundary>} />
+                                            <Route path="/group/:groupId?" render={(props) => <ErrorBoundary {...props}><GroupPage /></ErrorBoundary>} />
 
-                                        <Route path="/logs" render={(props) => <ErrorBoundary {...props}><LogsPage /></ErrorBoundary>} />
-                                        <Route path="/touchlink" render={(props) => <ErrorBoundary {...props}><TouchlinkPage /></ErrorBoundary>} />
-                                        <Route path="/dashboard" render={(props) => <ErrorBoundary {...props}><DashboardPage /></ErrorBoundary>} />
-                                        <Route path="/extensions" render={(props) => <ErrorBoundary {...props}><ExtensionsEditorPage /></ErrorBoundary>} />
-                                        <Route path="/" render={(props) => <ErrorBoundary {...props}><ConnectedZigbeePage /></ErrorBoundary>} />
-                                    </Switch>
-                                </div>
-                            </main>
-                        </div>
-                    </HashRouter>
-                </ThemeSwitcherProvider>
+                                            <Route path="/logs" render={(props) => <ErrorBoundary {...props}><LogsPage /></ErrorBoundary>} />
+                                            <Route path="/touchlink" render={(props) => <ErrorBoundary {...props}><TouchlinkPage /></ErrorBoundary>} />
+                                            <Route path="/dashboard" render={(props) => <ErrorBoundary {...props}><DashboardPage /></ErrorBoundary>} />
+                                            <Route path="/extensions" render={(props) => <ErrorBoundary {...props}><ExtensionsEditorPage /></ErrorBoundary>} />
+                                            <Route path="/" render={(props) => <ErrorBoundary {...props}><ConnectedZigbeePage /></ErrorBoundary>} />
+                                        </Switch>
+                                    </div>
+                                </main>
+                            </div>
+                        </HashRouter>
+                    </ThemeSwitcherProvider>
+                </GlobalModal>
             </Provider >
         </I18nextProvider>
     </Suspense>
