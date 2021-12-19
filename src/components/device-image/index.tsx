5dd7f7da272c30b6db26ece11eb2eb58e53d3e90
nurikk <ainur@yojee.com>
2021-12-19 17:30:29 +0800

    fix(DeviceImage): fix fallback logic

--- src/components/device-image/index.tsx
+++ src/components/device-image/index.tsx
@@ -5,63 +5,51 @@ import cx from "classnames";
 import { sanitizeZ2MDeviceName } from "../../utils";
 import style from "./style.css";
 import { useTranslation } from "react-i18next";
+import { useImage } from 'react-image'
+
 type DeviceImageProps = {
     device: Device;
     deviceStatus?: DeviceState;
     type?: "img" | "svg";
 }
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const genericDeviceImageFallback = (device: Device): string => genericDevice;
-const genSlsDeviceImageUrlZ2M = (device: Device): string => `https://www.zigbee2mqtt.io/images/devices/${sanitizeZ2MDeviceName(device?.definition?.model)}.jpg`;
-const converterDeviceImage = (device: Device): string | undefined => device.definition?.icon;
+const getFallbackDeviceImage = (device: Device): string => genericDevice;
+const getZ2mDeviceImage = (device: Device): string => `https://www.zigbee2mqtt.io/images/devices/${sanitizeZ2MDeviceName(device?.definition?.model)}.jpg`;
+const getConverterDeviceImage = (device: Device): string | undefined => device.definition?.icon;
 
 const sanitizeModelIDForImageUrl = (modelName: string): string => modelName?.replace("/", "_");
 
-export const genSlsDeviceImageUrlSLS = (device: Device): string => (`https://slsys.github.io/Gateway/devices/png/${sanitizeModelIDForImageUrl(device.model_id)}.png`);
+const getSlsDeviceImage = (device: Device): string => (`https://slsys.github.io/Gateway/devices/png/${sanitizeModelIDForImageUrl(device.model_id)}.png`);
 
 
 const AVAILABLE_GENERATORS = [
-    converterDeviceImage, genSlsDeviceImageUrlZ2M, genSlsDeviceImageUrlSLS
+    getConverterDeviceImage,
+    getZ2mDeviceImage,
+    getSlsDeviceImage,
+    getFallbackDeviceImage
 ]
 
 const DeviceImage: FunctionComponent<DeviceImageProps & ImgHTMLAttributes<HTMLDivElement | SVGImageElement>> = (props) => {
     const { t } = useTranslation("zigbee");
-    const [imageGenerators, setImageGenerators] = useState(AVAILABLE_GENERATORS);
+
     const { device = {} as Device, deviceStatus, type = "img", className, ...rest } = props;
-    let src: string | undefined = genericDeviceImageFallback(device);
-    const onImageError = () => {
-        const newGenerators = [...imageGenerators];
-        newGenerators.shift();
-        setImageGenerators(newGenerators);
-    };
+    const { src } = useImage({
+        srcList: AVAILABLE_GENERATORS.map(fn => fn(props.device)) as string[],
+    });
 
-    if (device?.definition?.model && imageGenerators.length) {
-        src = imageGenerators[0](device);
-        if (!src) {
-            onImageError();
-        }
-    }
-    if (src) {
-        const otaState = (deviceStatus?.update ?? {}) as OTAState;
-        const otaSpinner = otaState.state === "updating" ? <i title={t("updating_firmware")} className="fa fa-sync fa-spin position-absolute bottom-0 right-0" /> : null;
-        const interviewSpinner = device.interviewing ? <i title={t("interviewing")} className="fa fa-spinner fa-spin position-absolute bottom-0 right-0" /> : null;
-        const unsuccessfulInterview = !device.interviewing && !device.interview_completed ? <i title={t("interview_failed")} className="fa fa-exclamation-triangle position-absolute top-0 right-0 text-danger" /> : null;
-        switch (type) {
-            case "svg":
-                return <image crossOrigin={"anonymous"} {...rest} onError={onImageError} href={src} />;
-            case "img":
-            default:
-                return <div className={cx(className, "position-relative")} {...rest}>
-                    <img crossOrigin={"anonymous"} onError={onImageError} src={src} className={style.img} />
-                    {interviewSpinner}
-                    {otaSpinner}
-                    {unsuccessfulInterview}
-                </div>;
-        }
+    const otaState = (deviceStatus?.update ?? {}) as OTAState;
+    const otaSpinner = otaState.state === "updating" ? <i title={t("updating_firmware")} className="fa fa-sync fa-spin position-absolute bottom-0 right-0" /> : null;
+    const interviewSpinner = device.interviewing ? <i title={t("interviewing")} className="fa fa-spinner fa-spin position-absolute bottom-0 right-0" /> : null;
+    const unsuccessfulInterview = !device.interviewing && !device.interview_completed ? <i title={t("interview_failed")} className="fa fa-exclamation-triangle position-absolute top-0 right-0 text-danger" /> : null;
+    if (type === "svg") {
+        return <image crossOrigin={"anonymous"} {...rest} href={src} />;
     } else {
-        return null;
+        return <div className={cx(className, "position-relative")} {...rest}>
+            <img crossOrigin={"anonymous"} src={src} className={style.img} />
+            {interviewSpinner}
+            {otaSpinner}
+            {unsuccessfulInterview}
+        </div>;
     }
-
-
 }
 export default DeviceImage;
