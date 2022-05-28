6e670c0bfc19b04fc9d567d3561b997c54698f90
nurikk <ainur@yojee.com>
2022-05-29 00:32:58 +0800

    refactor(build): start ws server by vite

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,34 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import sassDts from "vite-plugin-sass-dts";
 import viteCompression from 'vite-plugin-compression';
+import { startServer } from './ws';
 
+export default defineConfig(async ({ command }) => {
+    if (command === 'serve') {
+        startServer();
+    }
 
-export default defineConfig({
-    root: 'src',
-    base: '',
-    build: {
-        emptyOutDir: true,
-        outDir: '../dist'
-    },
-    plugins: [
-        react({
-            include: '**/*.{jsx,tsx}',
-        }),
-        sassDts(),
-        viteCompression({verbose: true}),
-    ],
-    server: {
-        proxy: {
-            '/api': {
-                changeOrigin: true,
-                target: process.env.Z2M_API_URI ? process.env.Z2M_API_URI : "ws://localhost:8579",
-                ws: true
+    return {
+        root: 'src',
+        base: '',
+        build: {
+            emptyOutDir: true,
+            outDir: '../dist'
+        },
+        plugins: [
+            react({
+                include: '**/*.{jsx,tsx}',
+            }),
+            sassDts(),
+            viteCompression({ verbose: true }),
+        ],
+        server: {
+            proxy: {
+                '/api': {
+                    changeOrigin: true,
+                    target: process.env.Z2M_API_URI ? process.env.Z2M_API_URI : "ws://localhost:8579",
+                    ws: true
+                }
             }
         }
     }
--- ws.js
+++ ws.js
@@ -1,27 +1,37 @@
 const WebSocket = require("ws");
 const lineReader = require('line-reader');
 const fs = require("fs");
-const wss = new WebSocket.Server({
-    port: 8579,
-});
-
-wss.on("connection", (ws) => {
-
-    lineReader.eachLine('./ws-messages/onConnect.json', (line, last) => {
-        ws.send(line);
+function startServer() {
+    const wss = new WebSocket.Server({
+        port: 8579,
     });
 
-    ws.addEventListener("message", (message) => {
-        const msg = JSON.parse(message.data);
-        switch (msg.topic) {
-            case "bridge/request/networkmap":
-                ws.send(fs.readFileSync('./ws-messages/networkMapRequest.json', 'utf8'));
-                break;
-            case "bridge/request/touchlink/scan":
-                ws.send(fs.readFileSync('./ws-messages/onTouchlink.json', 'utf8'));
-                break;
-            default:
-                break;
-        }
+    wss.on("connection", (ws) => {
+
+        lineReader.eachLine('./ws-messages/onConnect.json', (line) => {
+            ws.send(line);
+        });
+
+        ws.addEventListener("message", (message) => {
+            const msg = JSON.parse(message.data);
+            switch (msg.topic) {
+                case "bridge/request/networkmap":
+                    ws.send(fs.readFileSync('./ws-messages/networkMapRequest.json', 'utf8'));
+                    break;
+                case "bridge/request/touchlink/scan":
+                    ws.send(fs.readFileSync('./ws-messages/onTouchlink.json', 'utf8'));
+                    break;
+                default:
+                    break;
+            }
+        });
     });
-});
+    console.log('started ws server');
+}
+
+
+if (require.main === module) {
+    startServer();
+}
+
+module.exports = { startServer };
