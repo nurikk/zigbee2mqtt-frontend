db8ee06baff6156d7a54b5b263ecd5606e98e548
Rotzbua <Rotzbua@users.noreply.github.com>
2023-10-05 14:40:44 +0200

    feat(eslint): add to GH Actions check

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,11 @@ module.exports = {
     ],
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'react/no-deprecated': 'warn',
         'react/prop-types': 'off',
     },
 };
