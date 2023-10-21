814cd8c9f06ae5ab5bf4ccd9e45bd428bfb79635
marcusds <git@marcusds.ca>
2023-10-21 11:21:13 -0700

    feat(device-page): fitler devices in select

---
+++ src/components/device-page/types.ts
@@ -0,0 +1,11 @@
+export type TabName =
+    | 'info'
+    | 'bind'
+    | 'state'
+    | 'exposes'
+    | 'clusters'
+    | 'reporting'
+    | 'settings'
+    | 'settings-specific'
+    | 'dev-console'
+    | 'scene';
