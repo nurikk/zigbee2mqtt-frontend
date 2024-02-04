2a66b1530b486be24aad0e10f96b9a751e82c19f
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-02-04 17:27:48 +0300

    fix(navbar): remove iframe check and use gap for button container

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -11,14 +11,8 @@ import { BridgeApi } from '../../actions/BridgeApi';
 import { ThemeSwitcher } from '../theme-switcher';
 import { WithTranslation, withTranslation } from 'react-i18next';
 import LocalePicker from '../../i18n/LocalePicker';
-import { isIframe } from '../../utils';
 import { StartStopJoinButton } from './StartStopJoinButton';
 
-import BootstrapButton from 'react-bootstrap/Button';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Collapse from 'react-bootstrap/esm/Collapse';
 import NavbarBrand from 'react-bootstrap/esm/NavbarBrand';
 
 const urls = [
@@ -71,19 +65,14 @@ const NavBar: FunctionComponent<PropsFromStore & ThemeActions & WithTranslation<
         setNavbarIsVisible(false);
     });
     return (
-        <nav className="navbar navbar-expand-lg">
+        <nav className={'navbar navbar-expand-lg'}>
             <div ref={ref as React.MutableRefObject<HTMLDivElement>} className="container-fluid align-items-baseline">
                 <NavbarBrand href="/#/">
-                    <img
-                        alt=""
-                        src="/images/logo.png"
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-
+                    <img alt={'Zigbee2MQTT'} src={'/images/logo.png'} width={'30'} height={'30'}
+                        className={'d-inline-block align-top'}
                     />{' '}
-                    <Link onClick={() => setNavbarIsVisible(false)} to="/">
-                        {isIframe() ? `Z2M@${document.location.hostname}` : 'Zigbee2MQTT'}
+                    <Link onClick={() => setNavbarIsVisible(false)} to={'/'}>
+                        {'Zigbee2MQTT'}
                     </Link>
                 </NavbarBrand>
                 <button
@@ -111,15 +100,15 @@ const NavBar: FunctionComponent<PropsFromStore & ThemeActions & WithTranslation<
                             </li>
                         ))}
                     </ul>
-                    <div className="d-flex align-self-start align-items-stretch flex-wrap justify-content-end">
+                    <div className="d-flex align-self-start align-items-center flex-wrap justify-content-end gap-1 py-2">
                         <StartStopJoinButton devices={devices} setPermitJoin={setPermitJoin} bridgeInfo={bridgeInfo} />
                         <LocalePicker />
                         <ThemeSwitcher />
                         {bridgeInfo.restart_required ? (
-                            <Button onClick={restartBridge} prompt className="btn btn-danger my-2 mx-1">
+                            <Button onClick={restartBridge} prompt className="btn btn-danger">
                                 {t('restart')}
                             </Button>
-                        ) : null}
+                         ) : null}
                     </div>
                 </div>
             </div>
