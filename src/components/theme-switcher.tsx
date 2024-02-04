fd2cdef44e748aaaa895673ce8282a649e056050
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-02-04 17:25:06 +0300

    feat(theme-switcher): turn into split dropdown and fix auto theme

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,30 +1,32 @@
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 export const ThemeSwitcher = (): JSX.Element => {
-    const getStoredTheme = () => localStorage.getItem('theme')
-    const setStoredTheme = theme => localStorage.setItem('theme', theme)
+    const { t } = useTranslation(['navbar']);
+    const getStoredTheme = () => localStorage.getItem('z2m-theme')
+    const setStoredTheme = theme => localStorage.setItem('z2m-theme', theme)
 
     const getPreferredTheme = () => {
         const storedTheme = getStoredTheme()
         if (storedTheme) {
             return storedTheme
         }
-
         return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
     }
 
     const setTheme = theme => {
-        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            document.documentElement.setAttribute('data-bs-theme', 'dark')
+        if (theme === 'auto') {
+            document.documentElement.setAttribute('data-bs-theme', window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
         } else {
             document.documentElement.setAttribute('data-bs-theme', theme)
         }
     }
 
     setTheme(getPreferredTheme())
-
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
         const storedTheme = getStoredTheme()
         if (storedTheme !== 'light' && storedTheme !== 'dark') {
@@ -35,38 +37,41 @@ export const ThemeSwitcher = (): JSX.Element => {
     const updateTheme = (theme: string) => {
         setStoredTheme(theme)
         setTheme(getPreferredTheme())
-        const icon = document.getElementById("theme-dropdown-icon")
+        const icon = document.getElementById('theme-dropdown-icon')
         icon!.className = getCurrentThemeClass()
     }
 
     const getCurrentThemeClass = () => {
         const theme = getStoredTheme()
-        let icon = "fa-solid me-1"
+        let icon = 'fa-solid'
         switch (theme) {
-            case 'light': { icon += " fa-sun"; break }
-            case 'dark': { icon += " fa-moon"; break }
-            default: { icon += " fa-circle-half-stroke"; break }
+            case 'light': { icon += ' fa-sun'; break }
+            case 'dark': { icon += ' fa-moon'; break }
+            default: { icon += ' fa-circle-half-stroke'; break }
         }
         return icon
     }
 
     return (
-        <Dropdown className={'my-2 ms-1'}>
-            <Dropdown.Toggle variant='outline-secondary' >
-                <i id="theme-dropdown-icon" className={getCurrentThemeClass()} style={{ width: '16px' }}></i>
+        <Dropdown as={ButtonGroup}>
+            <Button variant={'outline-secondary'} className={'d-flex align-items-center'}>
+                <i id={'theme-dropdown-icon'} className={getCurrentThemeClass()} style={{ margin: '0 1px', width: '16px' }}></i>
+            </Button>
+            <Dropdown.Toggle split={true} variant={'outline-secondary'} data-bs-reference={'parent'}>
+                <span className={'visually-hidden'}>{t('toggle_dropdown')}</span>
             </Dropdown.Toggle>
             <Dropdown.Menu className={'my-1'}>
                 <Dropdown.Item onClick={(e) => { updateTheme('light') }}>
-                    <i className={"fa-solid fa-sun me-2"} style={{ width: '16px' }}></i>
-                    <span className="">Light</span> {/* probably something like t('theme:light')*/}
+                    <i className={'fa-solid fa-sun me-2'} style={{ width: '16px' }}></i>
+                    <span>Light</span> {/* probably something like t('theme:light')*/}
                 </Dropdown.Item>
                 <Dropdown.Item onClick={(e) => { updateTheme('dark') }}>
-                    <i className={"fa-solid fa-moon me-2"} style={{ width: '16px' }}></i>
-                    <span className="">Dark</span> {/* probably something like t('theme:dark')*/}
+                    <i className={'fa-solid fa-moon me-2'} style={{ width: '16px' }}></i>
+                    <span>Dark</span> {/* probably something like t('theme:dark')*/}
                 </Dropdown.Item>
                 <Dropdown.Item onClick={(e) => { updateTheme('auto') }}>
-                    <i className={"fa-solid fa-circle-half-stroke me-2"} style={{ width: '16px' }}></i>
-                    <span className="">Auto</span> {/* probably something like t('theme:auto')*/}
+                    <i className={'fa-solid fa-circle-half-stroke me-2'} style={{ width: '16px' }}></i>
+                    <span>Auto</span> {/* probably something like t('theme:auto')*/}
                 </Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
