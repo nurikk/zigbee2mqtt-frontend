69047711f6384869ac1e381304532f911b6c8661
Ainur Timerbaev <ainur.timerbaev@superawesome.com>
2024-03-01 16:08:10 +0000

    fix(lint): fix linter

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -3,6 +3,7 @@ import { AllTheProviders } from './AllTheProviders';
 
 const customRender = (ui, options = {}) => render(ui, { wrapper: AllTheProviders, ...options });
 
+// eslint-disable-next-line react-refresh/only-export-components
 export * from '@testing-library/react';
 
 // override render method
