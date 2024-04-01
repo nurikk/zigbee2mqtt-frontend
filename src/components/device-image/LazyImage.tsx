0843cf2a7e9e4776cb8d1fce1ceeb7cab600c878
s33g <dev@charlie.fyi>
2024-04-01 02:40:52 +0100

    fix(animation): remove annoying image animation

--- src/components/device-image/LazyImage.tsx
+++ src/components/device-image/LazyImage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import style from './style.module.css';
 import { useImage } from 'react-image';
 import { AVAILABLE_GENERATORS } from '.';
 import { Device } from '../../types';
@@ -18,5 +17,5 @@ export function LazyImage(props: Readonly<LazyImageProps>) {
     if (type === 'svg') {
         return <image crossOrigin={'anonymous'} {...rest} href={src} />;
     }
-    return <img alt="" crossOrigin={'anonymous'} src={src} className={style.img} />;
+    return <img alt="" crossOrigin={'anonymous'} src={src} />;
 }
