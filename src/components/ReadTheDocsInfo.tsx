6994e27b58152d9c427dd704e55c0d5ab6b8ad78
Dawid Nowak <code@dnowak.pl>
2024-05-14 22:57:35 +0200

    style(DeviceSettings): whitespace
    `prettier --check "./**/*.{js,jsx,tsx}" -w`
    
    fixes https://github.com/nurikk/zigbee2mqtt-frontend/actions/runs/9082225515/job/24958031689
    
    I actually think it's more readable with the code being split into multiply lines…
    Not sure why the `prettier` did not like it ;p

--- src/components/ReadTheDocsInfo.tsx
+++ src/components/ReadTheDocsInfo.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 
-export const ReadTheDocsInfo = (props: {
-    docsUrl: string;
-}): JSX.Element => {
+export const ReadTheDocsInfo = (props: { docsUrl: string }): JSX.Element => {
     const { docsUrl } = props;
     return (
         <div className="card alert alert-info" role="alert">
             <div className="card-body">
                 <i className="fa-solid fa-circle-info fa-2xl me-2"></i>
 
-                <a
-                    href={docsUrl}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="alert-link align-middle"
-                >
+                <a href={docsUrl} target="_blank" rel="noreferrer" className="alert-link align-middle">
                     Read about this in the documentation…
                 </a>
             </div>
