2e2272f9ac9d7894f2311e8ee6867af510b8caa5
Dawid Nowak <code@dnowak.pl>
2024-05-14 10:28:06 +0200

    fix(DeviceSettings): docs info alert icon
    https://github.com/nurikk/zigbee2mqtt-frontend/pull/2020/commits/
    abc0b6e7a3e7413805d102d3ffc7768051e2a534#r1599289271

--- src/components/device-page/DeviceSettings.tsx
+++ src/components/device-page/DeviceSettings.tsx
@@ -57,16 +57,7 @@ export class DeviceSettings extends Component<DeviceSettingsProps, DeviceSetting
             <div>
                 <div className="card alert alert-info" role="alert">
                     <div className="card-body">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            fill="currentColor"
-                            className="bi bi-info-circle-fill flex-shrink-0 me-2"
-                            viewBox="0 0 16 16"
-                        >
-                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2" />
-                        </svg>
+                        <i className="fa-solid fa-circle-info fa-2xl me-2"></i>
 
                         <a
                             href="https://www.zigbee2mqtt.io/guide/configuration/devices-groups.html#common-device-options"
