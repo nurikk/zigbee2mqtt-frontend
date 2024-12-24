d8b950387e97b4bb5d4616f4b56504aad1541be0
Koen Kanters <koenkanters94@gmail.com>
2024-12-24 14:36:45 +0100

    fix(device_icon): remove jpg provider

--- src/components/device-image/index.tsx
+++ src/components/device-image/index.tsx
@@ -6,15 +6,12 @@ type ImageGeneratorFn = (device: Device) => string | undefined;
 const z2mBasePath = 'https://www.zigbee2mqtt.io/images/devices/';
 
 export const getZ2mDeviceImage = (device: Device): string =>
-    `${z2mBasePath}${sanitizeZ2MDeviceName(device?.definition?.model)}.jpg`;
-export const getZ2mDeviceImagePng = (device: Device): string =>
     `${z2mBasePath}${sanitizeZ2MDeviceName(device?.definition?.model)}.png`;
 const getConverterDeviceImage = (device: Device): string | undefined => device.definition?.icon;
 
 /* prettier-ignore */
 export const AVAILABLE_GENERATORS: ImageGeneratorFn[] = [
     getConverterDeviceImage,
-    getZ2mDeviceImagePng,
     getZ2mDeviceImage,
     () => genericDevice,
 ];
