396053b6e376b07af10f70ef3801798d95770b59
Ricc68 <ricc68@local>
2025-04-26 16:24:50 +0200

    fix: improved code and formatting

--- src/utils.ts
+++ src/utils.ts
@@ -32,12 +32,13 @@ export const toHHMMSS = (secs: number): string => {
 };
 
 export const fileVersion2String = (file_version: number): string => {
-    const a = (file_version & 0xff000000) >> 24;
-    const b = (file_version & 0x00ff0000) >> 16;
-    const c = (file_version & 0x0000ff00) >> 8;
-    const d = (file_version & 0x000000ff);
+    const versionString = file_version.toString(16).padStart(8, "0");
+    const appRelease = `${versionString[0]}.${versionString[1]}`;
+    const appBuild = versionString.slice(2, 4);
+    const stackRelease = `${versionString[4]}.${versionString[5]}`;
+    const stackBuild = versionString.slice(6);
 
-    return `${a.toString(10)}.${b.toString(10)}.${c.toString(10)}.${d.toString(10)}`;
+    return `${appRelease}.${appBuild}-${stackRelease}.${stackBuild}`;
 };
 
 export const lastSeen = (state: DeviceState, lastSeenType: LastSeenType): Date | undefined => {
